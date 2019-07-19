bc9b2d33f973476464155d5e1875c66de8c7144b
tjhooper1 <tomh386@gmail.com>
2019-07-18 22:10:20 -0400

    added auth subscription to app.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,41 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up /sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={SignInAndSignUp}/>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsbscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsbscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount(){
+    this.unsbscribeFromAuth();
+  }
+
+  render(){
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/shop' component={ShopPage}/>
+          <Route path='/signin' component={SignInAndSignUp}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
