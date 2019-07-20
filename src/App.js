d0532e747b9e90f96bcb24d062ea5f469896184d
tjhooper1 <tomh386@gmail.com>
2019-07-20 12:59:59 -0400

    added firestore for database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up /sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -19,8 +19,8 @@ class App extends React.Component {
   unsbscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsbscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsbscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user)
     });
   }
 
