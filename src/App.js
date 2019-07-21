29bcfcf509e419f193d518e2aa184103dbdea405
tjhooper1 <tomh386@gmail.com>
2019-07-21 16:06:09 -0400

    added some auth

--- src/App.js
+++ src/App.js
@@ -19,8 +19,20 @@ class App extends React.Component {
   unsbscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsbscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user)
+    this.unsbscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
+      this.setState({currentUser: userAuth});
     });
   }
 
