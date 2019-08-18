37e90898753fa83b028221e8e27d74f1c65b8568
tjhooper1 <tomh386@gmail.com>
2019-08-18 18:57:45 -0400

    added more features to checkout item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -8,8 +8,9 @@ import CartItem from '../cart-item/cart-item.component';
 
 import CustomButton from '../custom-button/custom-button.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-const CartDropdown = ({ cartItems, history }) => {
+const CartDropdown = ({ cartItems, history, dispatch }) => {
     return (
         <div className='cart-dropdown'>
             <div className='cart-items'>
@@ -21,7 +22,12 @@ const CartDropdown = ({ cartItems, history }) => {
             : <span className="empty-message">Your cart is empty</span>
         }
             </div>
-            <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+            <CustomButton onClick={() => {
+                history.push('/checkout')
+                dispatch(toggleCartHidden())
+                }
+            }>GO TO CHECKOUT
+            </CustomButton>
         </div>
     )
 }
